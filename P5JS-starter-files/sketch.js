1a54e2d96bc6ae6d1b8debcec11ea606af19d68f
Peter Wildman <peterwild246@gmail.com>
2020-03-17 19:03:04 +1100

    Add files via upload

---
+++ P5JS-starter-files/sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  // put setup code here
+  var canv = createCanvas(960,540);
+  // make div#canvas-container the parent of the created canvas
+  canv.parent("canvas-container");
+  background(100);
+}
+
+function  draw() {
+  // put drawing code here
+  
+}
